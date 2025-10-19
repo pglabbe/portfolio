7e256da9ac4de6e1071b1273a76bd62179e3230b
pglabbe <piergabriel.labbe@gmail.com>
2025-10-18 21:32:34 -0400

    Improved the general structure of the site, improved the texts and vibe, added smooth scroll

--- app.js
+++ app.js
@@ -1,161 +1,151 @@
 const screen = PetiteVue.reactive({
-    width: window.innerWidth,
-    mobile: window.innerWidth <= 900
+  width: window.innerWidth,
+  mobile: window.innerWidth <= 900
 });
 
 window.addEventListener("resize", () => {
-    screen.width = window.innerWidth;
-    screen.mobile = window.innerWidth <= 900;
+  screen.width = window.innerWidth;
+  screen.mobile = window.innerWidth <= 900;
 });
+
+const lenis = new Lenis({ autoRaf: true, anchors: true });
+const lenis_about_modal = new Lenis({ wrapper: document.getElementById("about-modal"), content: document.querySelector(".modal-content"), autoRaf: true });
+const lenis_project_modal = new Lenis({ wrapper: document.getElementById("project-modal"), content: document.querySelector(".modal-content"), autoRaf: true });
 
 PetiteVue.createApp({
-    $screen: screen,
+  $screen: screen,
 
-    allowScroll()
-    {
-        document.querySelector("body").classList.remove("no-scroll");
-    },
-    preventScroll()
-    {
-        if(!document.querySelector("body").classList.contains("no-scroll")) {
-            document.querySelector("body").classList.add("no-scroll");
-        }
-    },
-
-    menu: false,
-    menuItems: [
-        {
-            key: "menu.home",
-            link: "#"
-        },
-        {
-            key: "menu.about",
-            link: "#about"
-        },
-        {
-            key: "menu.projects",
-            link: "#projects"
-        },
-        {
-            key: "menu.contact",
-            link: "#contact"
-        }
-    ],
-
-    toggleMenu()
-    {
-        this.menu = !this.menu;
-        
-        if(this.menu) {
-            this.preventScroll();
-        } else {
-            this.allowScroll();
-        }
-    },
-    closeMenu()
-    {
-        this.menu = false;
-        this.allowScroll();
-    },
-
-    contactLink: "mailto:piergabriel.labbe@gmail.com",
-
-    aboutOpened: false,
-    openAbout()
-    {
-        document.getElementById("about-modal").scrollTop = 0;
-        
-        this.aboutOpened = true;
-        this.preventScroll();
-    },
-    closeAbout()
-    {
-        this.aboutOpened = false;
-        this.allowScroll();
-    },
-    
-    projectOpened: false,
-    selectedProject: null,
-
-    projects: [
-        {
-            key: "saveUp",
-            color: "#08877e",
-            mobile: true,
-            year: "2024",
-            tags: ["mobile", "designUIX"],
-            techs: ["Figma"],
-            images: [
-                {src: `mockup-fr.jpg`, alt: "Mockup"},
-                {src: "logo.jpg", alt: "Logo"}
-            ],
-            link: "https://www.figma.com/proto/VZUOdx8wfN1NdFOGYB6SJK/SaveUp",
-            prototypeLink: "https://www.figma.com/design/VZUOdx8wfN1NdFOGYB6SJK/SaveUp"
-        },
-        {
-           key: "saveUpWebsite",
-           color: "#333333",
-           year: "2024",
-           tags: ["website", "mobile", "designUIX"],
-           techs: ["Figma"],
-           images: [
-               {src: `mockup-fr.jpg`, alt: "Mockup"}
-           ],
-           link: "https://www.figma.com/proto/R8usq9CPmiUgy1QaaqLKRU/SaveUp-Website",
-           prototypeLink: "https://www.figma.com/design/R8usq9CPmiUgy1QaaqLKRU/SaveUp-Website"
-        },
-        {
-            key: "traceApp",
-            color: "#012141",
-            year: "2022 - 2024",
-            tags: ["app", "designUIX", "web"],
-            techs: ["Vue.js", "PHP"],
-            images: [
-                {src: `login-${$locale}.jpg`, alt: "Login"},
-                {src: `dashboard-${$locale}.jpg`, alt: "Dashboard"},
-                {src: `reported-events-${$locale}.jpg`, alt: "Reported Events"},
-                {src: `reports-and-actions-${$locale}.jpg`, alt: "Reports and Actions"}
-            ],
-            link: "https://playground.trace.iczones.app"
-        },
-        {
-            key: "traceWebsite",
-            color: "#f2430a",
-            year: "2022 - 2024",
-            tags: ["website", "designUIX", "web"],
-            techs: ["Wordpress"],
-            images: [
-                {src: `website-${$locale}.jpg`, alt: "Website"}
-            ],
-            link: "https://traceapp.ca"
-        },
-    ],
-
-    openProject(project)
-    {
-        this.selectedProject = project;
-        document.getElementById("project-modal").scrollTop = 0;
-        
-        this.projectOpened = true;
-        this.preventScroll();
-    },
-    closeProject()
-    {
-        this.projectOpened = false;
-        this.allowScroll();
-    },
-
-    getProjectAnimationDelay(index)
-    {
-        if(screen.mobile || index >= 2) {
-            return index % 2 === 0 ? 0 : 100;
-        }
-
-        return index === 1 ? 500 : 400;
+  allowScroll() {
+    document.querySelector("body").classList.remove("no-scroll");
+  },
+  preventScroll() {
+    if (!document.querySelector("body").classList.contains("no-scroll")) {
+      document.querySelector("body").classList.add("no-scroll");
     }
-}).mount();
+  },
 
-AOS.init({
-    duration: 600,
-    easing: "ease-out"
-});
+  menu: false,
+  menuItems: [
+    {
+      key: "menu.home",
+      link: "#"
+    },
+    {
+      key: "menu.about",
+      link: "#about"
+    },
+    {
+      key: "menu.projects",
+      link: "#projects"
+    },
+    {
+      key: "menu.contact",
+      link: "#contact"
+    }
+  ],
+
+  toggleMenu() {
+    this.menu = !this.menu;
+
+    if (this.menu) {
+      this.preventScroll();
+    } else {
+      this.allowScroll();
+    }
+  },
+  closeMenu() {
+    this.menu = false;
+    this.allowScroll();
+  },
+
+  contactLink: "mailto:piergabriel.labbe@gmail.com",
+
+  aboutOpened: false,
+  openAbout() {
+    document.getElementById("about-modal").scrollTop = 0;
+
+    this.aboutOpened = true;
+    this.preventScroll();
+  },
+  closeAbout() {
+    this.aboutOpened = false;
+    this.allowScroll();
+  },
+
+  projectOpened: false,
+  selectedProject: null,
+
+  projects: [
+    {
+      key: "saveUp",
+      color: "#08877e",
+      mobile: true,
+      year: "2024",
+      tags: ["mobile", "designUIX"],
+      techs: ["Figma"],
+      images: [
+        { src: `mockup-fr.jpg`, alt: "Mockup" },
+        { src: "logo.jpg", alt: "Logo" }
+      ],
+      link: "https://www.figma.com/proto/VZUOdx8wfN1NdFOGYB6SJK/SaveUp",
+      prototypeLink: "https://www.figma.com/design/VZUOdx8wfN1NdFOGYB6SJK/SaveUp"
+    },
+    {
+      key: "saveUpWebsite",
+      color: "#333333",
+      year: "2024",
+      tags: ["website", "mobile", "designUIX"],
+      techs: ["Figma"],
+      images: [
+        { src: `mockup-fr.jpg`, alt: "Mockup" }
+      ],
+      link: "https://www.figma.com/proto/R8usq9CPmiUgy1QaaqLKRU/SaveUp-Website",
+      prototypeLink: "https://www.figma.com/design/R8usq9CPmiUgy1QaaqLKRU/SaveUp-Website"
+    },
+    {
+      key: "traceApp",
+      color: "#012141",
+      year: "2022 - 2024",
+      tags: ["app", "designUIX", "web"],
+      techs: ["Vue.js", "PHP"],
+      images: [
+        { src: `login-${$locale}.jpg`, alt: "Login" },
+        { src: `dashboard-${$locale}.jpg`, alt: "Dashboard" },
+        { src: `reported-events-${$locale}.jpg`, alt: "Reported Events" },
+        { src: `reports-and-actions-${$locale}.jpg`, alt: "Reports and Actions" }
+      ],
+      link: "https://playground.trace.iczones.app"
+    },
+    {
+      key: "traceWebsite",
+      color: "#f2430a",
+      year: "2022 - 2024",
+      tags: ["website", "designUIX", "web"],
+      techs: ["Wordpress"],
+      images: [
+        { src: `website-${$locale}.jpg`, alt: "Website" }
+      ],
+      link: "https://traceapp.ca"
+    },
+  ],
+
+  openProject(project) {
+    this.selectedProject = project;
+    document.getElementById("project-modal").scrollTop = 0;
+
+    this.projectOpened = true;
+    this.preventScroll();
+  },
+  closeProject() {
+    this.projectOpened = false;
+    this.allowScroll();
+  },
+
+  getProjectAnimationDelay(index) {
+    if (screen.mobile || index >= 2) {
+      return index % 2 === 0 ? 0 : 100;
+    }
+
+    return index === 1 ? 500 : 400;
+  }
+}).mount();
---
+++ scripts/translator.js
@@ -0,0 +1,161 @@
+const $allTranslations = {
+  fr: {
+    title: "PGL - Designer UI/UX, Développeur Web",
+    description: "Bienvenue, moi c'est Pier-Gabriel, je suis designer UI/UX et développeur web, je transforme vos idées en projet réel.",
+
+    localeSwitch: "English",
+    language: {
+      fr: "Français",
+      en: "Anglais"
+    },
+    menu: {
+      home: "Accueil",
+      about: "À propos",
+      projects: "Projets",
+      contact: "Contact"
+    },
+
+    action: {
+      contactMe: "Contactez-moi"
+    },
+
+    intro: {
+      title: "Bienvenue, moi c'est Pier-Gabriel",
+      text: "<p>Je suis <strong>développeur web</strong>, curieux et passionné par le <strong>design d'interface</strong>.</p><p>J'aime <strong>apprendre</strong>, <strong>créer</strong> et <strong>construire</strong> des projets qui apportent quelque chose de concret.</p>"
+    },
+
+    about: {
+      title: "À propos de moi",
+      text: "<p>Avec plus de 6 ans d'expérience en <strong>développement web</strong>, j'ai eu la chance de travailler sur toutes sortes de projets. C'est ce parcours qui m'a donné le goût du <strong>design</strong> et du souci du <strong>détail</strong>.</p>",
+      action: "Voir mon parcours",
+
+      modalTitle: "Mon parcours",
+      education: "ÉDUCATION",
+      experience: "EXPÉRIENCE",
+      skills: "COMPÉTENCES",
+
+      design: "Design",
+      tools: "Outils",
+      coding: "Programmation",
+      languages: "Langues"
+    },
+
+    journey: {
+      googleUX: {
+        title: "Google UX Design Certificate",
+        location: "Coursera - En ligne",
+        date: "Oct. 2024 - Nov. 2024"
+      },
+      cba: {
+        title: "Technique de l'informatique",
+        location: "Cégep Beauce-Appalache",
+        date: "Août 2017 - Juin 2020"
+      },
+      fatfish: {
+        title: "Développeur Web",
+        location: "fatfish - Temps plein",
+        date: "Janv. 2025 - Présent"
+      },
+      icz: {
+        title: "Développeur Web",
+        location: "IC Zones - Temps plein",
+        date: "Juin 2020 - Nov. 2024"
+      },
+      iczStage: {
+        title: "Stages - Développeur Web",
+        location: "IC Zones - Temps partiel",
+        date: "Mai 2018 - Juin 2020"
+      }
+    },
+
+    project: {
+      saveUp: {
+        title: "SaveUp",
+        subtitle: "Application mobile de gestion de budgets",
+        about: "<p><strong>SaveUp</strong> est une application mobile conçue pour simplifier la gestion de budgets communs et encourager les économies en groupe. Que ce soit pour planifier des vacances de rêve, acheter un cadeau commun, ou partager les dépenses quotidiennes d'un foyer, <strong>SaveUp</strong> permet aux familles et amis de collaborer efficacement et de suivre leurs finances en temps réel.</p>",
+        work: "<p><strong>SaveUp</strong> est un projet initié dans le cadre de mon certificat, où j'ai travaillé sur la conception de certaines pages de l'application. Ce projet m'a permis d'approfondir mes compétences en <strong>design UI/UX</strong> et de me familiariser davantage avec <strong>Figma</strong>, tout en travaillant sur un concept visuellement impactant et orienté vers les besoins des utilisateurs.</p>"
+      },
+      saveUpWebsite: {
+        title: "Site Web de SaveUp",
+        subtitle: "Pour une application mobile de gestion de budgets",
+        about: "<p><strong>SaveUp</strong> est une application mobile conçue pour simplifier la gestion de budgets communs et encourager les économies en groupe. Que ce soit pour planifier des vacances de rêve, acheter un cadeau commun, ou partager les dépenses quotidiennes d'un foyer, <strong>SaveUp</strong> permet aux familles et amis de collaborer efficacement et de suivre leurs finances en temps réel.</p>",
+        work: "<p>Le site Web de <strong>SaveUp</strong> est la suite du projet initié dans le cadre de mon certificat, où j'ai réalisé un site Web démonstratif pour l'application mobile. Ce projet m'a permis d'approfondir mes compétences en <strong>design UI/UX</strong> et de me familiariser davantage avec <strong>Figma</strong>, tout en travaillant sur un concept visuellement impactant et orienté vers les besoins des utilisateurs.</p>"
+      },
+      traceApp: {
+        title: "Application Trace",
+        subtitle: "Application de gestion de suivis",
+        about: "<p><strong>Trace</strong> est l'outil par excellence pour signaler et prendre en charge rapidement toutes situations qui nécessitent un suivi. <strong>Trace</strong> centralise à un seul endroit les suivis à faire pour éviter d'en échapper. Il peut être facilement utilisé pas tous vos employés sur le terrain (même ceux qui n'ont qu'un cellulaire) pour rapporter des situations qui requièrent une prise en charge.</p>",
+        work: "<p><strong>Trace</strong> est une application web sur laquelle j'ai travaillé pendant deux ans en tant que développeur web. Mon rôle couvrait à la fois le <strong>back-end</strong> et le <strong>front-end</strong>, mais c'est grâce à ce projet que j'ai découvert mon attrait pour le <strong>front-end</strong> et, plus encore, pour le <strong>design UI/UX</strong>.</p><p>Au fil du projet, j'ai entrepris une <strong>refonte majeure</strong> de l'application pour la rendre plus fonctionnelle sur <strong>mobile</strong> et pour améliorer son ergonomie. Plusieurs sections ont été redessinées afin d'offrir une navigation plus intuitive et adaptée aux besoins des utilisateurs.</p><p>Ce projet m'a permis d'approfondir mes compétences en <strong>développement web</strong> tout en me spécialisant davantage dans la conception d'interfaces utilisateur modernes et optimisées.</p>"
+      },
+      traceWebsite: {
+        title: "Site Web de Trace",
+        subtitle: "Pour une application de gestion de suivis",
+        about: "<p><strong>Trace</strong> est l'outil par excellence pour signaler et prendre en charge rapidement toutes situations qui nécessitent un suivi. <strong>Trace</strong> centralise à un seul endroit les suivis à faire pour éviter d'en échapper. Il peut être facilement utilisé pas tous vos employés sur le terrain (même ceux qui n'ont qu'un cellulaire) pour rapporter des situations qui requièrent une prise en charge.</p>",
+        work: "<p>Dans le cadre du projet <strong>Trace</strong>, j'ai également conçu et réalisé le <strong>site web</strong> de présentation de l'application. Mon travail a consisté à développer un site vitrine attractif et informatif, mettant en avant les fonctionnalités principales et les avantages de <strong>Trace</strong> de manière claire et engageante.</p><p>J'ai assuré la conception visuelle et le développement avec <strong>Wordpress</strong>, en veillant à intégrer des éléments de design cohérents avec l'identité de l'application. L'objectif était de transmettre efficacement la valeur ajoutée de <strong>Trace</strong> tout en offrant une expérience utilisateur fluide, que ce soit sur ordinateur ou mobile.</p><p>Ce projet m'a permis de consolider mes compétences en création de <strong>sites web</strong> et d'explorer davantage la dimension marketing du <strong>design UI/UX</strong>.</p>"
+      },
+
+      year: "Année(s)",
+      usedTech: "Technologies utilisées",
+
+      view: "Voir le projet",
+      viewPrototype: "Voir le prototype",
+
+      workTogether: {
+        title: "Travaillons ensemble!",
+        text: "Vous aimez mon travail et vous souhaitez quelque chose de similaire?"
+      }
+    },
+
+    tag: {
+      app: "Application",
+      website: "Site web",
+      mobile: "Mobile",
+      designUIX: "Design UI/UX",
+      dev: "Développement",
+      web: "Développement web"
+    },
+
+    footer: {
+      title: "Vous avez une idée en tête?",
+      text: "<p>Réalisons-la <strong>ensemble</strong>!</p><p>Chaque <strong>détail</strong> compte pour donner vie à une <strong>idée</strong> qui reste en tête.</p>"
+    },
+  }
+};
+let $translations = {};
+
+const locales = Object.keys($allTranslations);
+const $locale = localStorage.getItem("lang") && locales.includes(localStorage.getItem("lang")) ? localStorage.getItem("lang") : "fr";
+
+const switchLocale = () => {
+  const nextLocale = $locale === "fr" ? "en" : "fr";
+  localStorage.setItem("lang", locales.includes(nextLocale) ? nextLocale : $locale);
+}
+
+const parseTrans = (_object, _path = '') => {
+  Object.entries(_object).forEach(([_key, _value]) => {
+    let fullPath = _path ? [_path, _key].join('.') : _key;
+    if (typeof _value === 'string') {
+      $translations[fullPath] = _value;
+    } else {
+      parseTrans(_value, fullPath);
+    }
+  });
+}
+parseTrans($allTranslations[$locale]);
+
+const $trans = (_id, _params = {}) => {
+  let text = $translations[_id];
+  if (text) {
+    Object.entries(_params).forEach(([_key, _value]) => {
+      text = text.replace(`{${_key}}`, _value);
+    });
+
+    return text;
+  }
+
+  return '';
+}
+
+document.documentElement.lang = $locale;
+document.title = $trans("title");
+document.querySelector("meta[name='description']").setAttribute("content", $trans("description"));
